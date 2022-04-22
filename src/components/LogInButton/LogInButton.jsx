3abf8a28609d33382b633350257eb86636c69a17
Natalie Lovibond Hodgson <90227438+smllygrl@users.noreply.github.com>
2022-04-22 12:26:49 +1000

    navigation works

--- src/components/LogInButton/LogInButton.jsx
+++ src/components/LogInButton/LogInButton.jsx
@@ -1,10 +1,6 @@
 import { Link } from "react-router-dom";
 
 const LogInButton = ({ buttonName }) => {
-  // function: cheks if valid user
-  // takes to homepage if yes
-  // return error if no
-  // has the props of a loginForm
   return (
     <Link to="/user">
       <button>{buttonName}</button>
--- src/pages/LoggedInPage/LoggedInPage.jsx
+++ src/pages/LoggedInPage/LoggedInPage.jsx
@@ -4,13 +4,6 @@ import CreateListButton from "../../components/CreateListButton/CreateListButton
 import BaseFormat from "../../styling/format/BaseFormat/BaseFormat";
 
 const LoggedInPage = () => {
-  // ADMIN PANEL:
-  // user greeting
-  // sign out button (nav to log in page)
-
-  // DISPLAY:
-  // CREATE button (new list)
-
   return (
     <BaseFormat
       adminPanelContent={
