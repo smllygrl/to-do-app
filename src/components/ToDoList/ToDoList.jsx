1cd199d2950070314a7dd7faaf9f3143c7bf6889
Natalie Lovibond Hodgson <90227438+smllygrl@users.noreply.github.com>
2022-04-22 13:16:29 +1000

    simple ul and li to do list creates

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,10 +1,43 @@
-const ToDoList = ({ title }) => {
-  // renders on create
-  //
+import { useState, useEffect } from "react";
+
+const ToDoList = () => {
+  const [toDoList, setToDoList] = useState([]);
+  const [toDo, setToDo] = useState("");
+
+  const handleChange = (event) => {
+    setToDo(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (toDo !== "") {
+      setToDoList([
+        ...toDoList,
+        {
+          id: toDoList.length + 1,
+          text: toDo,
+        },
+      ]);
+    }
+    setToDo("");
+  };
+
   return (
     <div>
-      <h3>{title}</h3>
-      <ul></ul>
+      <h3>To Do List</h3>
+      <input
+        name="toDoItem"
+        type="text"
+        placeholder="Create a new 'to do' item!"
+        value={toDo}
+        onChange={handleChange}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {toDoList.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 };
