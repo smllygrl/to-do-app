98634b1cdfdf2ad5ee530b343a9839b79b077857
Natalie Lovibond Hodgson <90227438+smllygrl@users.noreply.github.com>
2022-04-27 09:29:01 +1000

    more modularisation

--- src/styling/formats/SignUpFormat/SignUpFormat.jsx
+++
@@ -1,9 +0,0 @@
-const SignUpFormat = ({ content }) => {
-  return (
-    <div className="signUpFormat">
-      <div className="signUpFormat__content">{content}</div>
-    </div>
-  );
-};
-
-export default SignUpFormat;
